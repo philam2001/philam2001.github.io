fbec078f70d4e8dbc351fd62eaf31bf64600682a
Phi Lam <lamyenphi14@gmail.com>
2025-01-05 23:46:57 -0800

    Delete dropdown to fix deploy issue

--- web/src/components/Experience.js
+++ web/src/components/Experience.js
@@ -5,11 +5,11 @@ import fe from '../assets/img/fe.png'
 import { useState } from 'react'
 
 export const Experience = () => {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    // const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    const toggleDropdown = () => {
-        setIsDropdownOpen(!isDropdownOpen);
-    };
+    // const toggleDropdown = () => {
+    //     setIsDropdownOpen(!isDropdownOpen);
+    // };
 
     const experiences = [
         {
@@ -136,45 +136,6 @@ export const Experience = () => {
                                             </ul> */}
                                 </div>
                             ))}
-                            {exp.company === "UBC Formula Electric" && (
-                                <>
-                                    <button onClick={toggleDropdown} className="dropdown-button">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 330.002 330.002"
-                                            className={`dropdown-icon ${isDropdownOpen ? "flipped" : ""}`}
-                                            width="20px"
-                                            height="20px"
-                                        >
-                                            <path
-                                                d="M324.001,209.25L173.997,96.75c-5.334-4-12.667-4-18,0L6.001,209.25c-6.627,4.971-7.971,14.373-3,21
-                                                c2.947,3.93,7.451,6.001,12.012,6.001c3.131,0,6.29-0.978,8.988-3.001L164.998,127.5l141.003,105.75c6.629,4.972,16.03,3.627,21-3
-                                                C331.972,223.623,330.628,214.221,324.001,209.25z"
-                                                fill="currentColor"
-                                            />
-                                        </svg>
-                                    </button>
-                                    {isDropdownOpen && (
-                                        <div className="formula-description">
-                                            <p>
-                                                Formula Electric is a student-led team that designs and builds high-performance electric race cars
-                                                for the Formula SAE Electric competition. The team’s vehicles feature advanced telemetry, data handling,
-                                                and custom industry-level electric systems, requiring cross-functional collaboration to ensure seamless
-                                                integration and reliable performance.
-                                            </p>
-                                            <p>
-                                                Our repository is an open-source project where we contribute software for telemetry, firmware for custom PCBs, control system simulations in MATLAB, custom file systems, and more!
-                                            </p>
-
-                                            <div className='fe-link-container'>
-                                                <a href="https://github.com/UBCFormulaElectric/Consolidated-Firmware" target="_blank" rel="noopener noreferrer" className="fe-github-link">
-                                                    🌟 Explore our GitHub Repository
-                                                </a>
-                                            </div>
-                                        </div>
-                                    )}
-                                </>
-                            )}
                         </div>
                     </div>
                 ))}
